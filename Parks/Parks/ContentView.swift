0c41dd0a30b612f25cde77d40b29afcbbed93235
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-10-22 16:57:37 -0600

    Step 3 and 4 Complete
    Now see a visual list of all parks

--- Parks/Parks/ContentView.swift
+++ Parks/Parks/ContentView.swift
@@ -8,46 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var parks: [Park] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVStack {
+                ForEach(parks) { park in
+                    Text(park.name)
+                        .font(.title)
+                }
+            }
         }
         .padding()
         .onAppear(perform: {
-            
-            // Create a Task instance
             Task {
-                
-                // URL for the API endpoint
-                // 👋👋👋 Make sure to replace {YOUR_API_KEY} in the URL with your actual NPS API Key
-                // Pass in any state code you like for the stateCode parameter. For instance, stateCode=fl (Florida)
-                let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key=rGTJp1efbBaY0HR1Oed1hcQQvxoUyiR1BycbwVGi")!
-                
-                // Wrap in do/catch since URLSession async can throw errors
-                do {
-                    
-                    // Perform an asynchronous data request using URLSession
-                    let (data, _) = try await URLSession.shared.data(from: url)
-                    
-                    // Print the JSON
-                    // Convert data to JSON object
-                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-                    
-                    // Convert JSON object back to pretty-printed data
-                    let prettyPrintedData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
-                    
-                    // Convert data to string for printing
-                    if let prettyPrintedString = String(data: prettyPrintedData, encoding: .utf8) {
-                        print(prettyPrintedString)
-                    }
-                } catch {
-                    print(error.localizedDescription)
-                }
+                await fetchParks()
             }
         })
+    }
+
+    private func fetchParks() async {
+        // URL for the API endpoint
+        // 👋👋👋 Make sure to replace {YOUR_API_KEY} in the URL with your actual NPS API Key
+        let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=rGTJp1efbBaY0HR1Oed1hcQQvxoUyiR1BycbwVGi")!
+        do {
+
+            // Perform an asynchronous data request
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // Decode json data into ParksResponse type
+            let parksResponse = try JSONDecoder().decode(ParksResponse.self, from: data)
+
+            // Get the array of parks from the response
+            let parks = parksResponse.data
+
+            // Print the full name of each park in the array
+            for park in parks {
+                print(park.fullName)
+            }
+
+            // Set the parks state property
+            self.parks = parks
+
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
